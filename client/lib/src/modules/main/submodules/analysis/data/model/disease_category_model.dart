b5a9c846a4c541a9897f1871e5710ebd84a6ff9b
Marcelo Moresco <marcelo.moresco@koin.com.br>
2024-11-22 09:08:57 -0300

    feat: transalte imte

--- client/lib/src/modules/main/submodules/analysis/data/model/disease_category_model.dart
+++ client/lib/src/modules/main/submodules/analysis/data/model/disease_category_model.dart
@@ -45,4 +45,49 @@ class DiseaseCategoryMapper {
         return null;
     }
   }
+
+  static String? translate(DiseaseCategory category) {
+    switch (category) {
+      case DiseaseCategory.acneAndRosaceaPhotos:
+        return 'Acne e Rosácea';
+      case DiseaseCategory.actinicKeratosisBasalCellCarcinoma:
+        return 'Queratose Actínica, Carcinoma Basocelular ou outras Lesões Malignas';
+      case DiseaseCategory.atopicDermatitisPhotos:
+        return 'Fotos de Dermatite Atópica';
+      case DiseaseCategory.cellulitisImpetigoAndBacterialInfections:
+        return 'Celulite, Impetigo ou outras Infecções Bacterianas';
+      case DiseaseCategory.eczemaPhotos:
+        return 'Eczema';
+      case DiseaseCategory.exanthemsAndDrugEruptions:
+        return 'Exantemas e Erupções Medicamentosas';
+      case DiseaseCategory.herpesHPVAndSTDsPhotos:
+        return 'Herpes, HPV ou outras DSTs';
+      case DiseaseCategory.lightDiseasesAndPigmentationDisorders:
+        return 'Doenças de Luz ou Distúrbios de Pigmentação';
+      case DiseaseCategory.lupusAndConnectiveTissueDiseases:
+        return 'Lúpus ou Outras Doenças do Tecido Conjuntivo';
+      case DiseaseCategory.melanomaSkinCancerNeviAndMoles:
+        return 'Melanoma, Câncer de Pele, Nevos ou Melanócitos';
+      case DiseaseCategory.poisonIvyAndContactDermatitis:
+        return 'Hera Venenosa ou Outras Dermatites de Contato';
+      case DiseaseCategory.psoriasisAndLichenPlanus:
+        return 'Psoríase, Líquen Plano ou Doenças Relacionadas';
+      case DiseaseCategory.seborrheicKeratosesAndBenignTumors:
+        return 'Queratoses Seborreicas ou Outros Tumores Benignos';
+      case DiseaseCategory.systemicDisease:
+        return 'Doença Sistêmica';
+      case DiseaseCategory.tineaRingwormAndFungalInfections:
+        return 'Tínea, Micose, Candidíase ou Outras Infecções Fúngicas';
+      case DiseaseCategory.urticariaHives:
+        return 'Urticária (Erupções)';
+      case DiseaseCategory.vascularTumors:
+        return 'Tumores Vasculares';
+      case DiseaseCategory.vasculitisPhotos:
+        return 'Vasculite';
+      case DiseaseCategory.wartsMolluscumAndViralInfections:
+        return 'Verrugas, Moluscos ou Outras Infecções Virais';
+      default:
+        return null;
+    }
+  }
 }
